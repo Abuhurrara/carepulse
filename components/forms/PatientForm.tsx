aed358688f6f5ef0eff4e8d5b6c69bb2afa300ab
Abuhurrara <abuhurrara1093@gmail.com>
2025-04-09 00:49:55 +0500

    add ofrm fields and phone fields

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -7,6 +7,16 @@ import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import CustomFormField from "../ui/CustomFormField";
 
+export enum FormFieldType {
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneinput",
+  CHECKBOX = "checkboz",
+  DATE_PICKER = "datePicker",
+  SELECT = "select",
+  SEKELTON = "sekelton", 
+}
+
 const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
@@ -31,7 +41,31 @@ const PatientForm = () => {
           <h1 className="header ">Hi there 👋</h1>
           <p className="text-dark-700">Schedule your first appointment.</p>
         </section>
-        <CustomFormField control={form.control} />
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.INPUT}
+          name="name"
+          label="Full name"
+          placeholder="Abu Hurrara"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+         <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.INPUT}
+          name="email"
+          label="Email"
+          placeholder="abuhurrara@gmail.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+         <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.PHONE_INPUT}
+          name="phone"
+          label="Phone number"
+          placeholder="(521) 123-4567"
+        />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
--- components/ui/CustomFormField.tsx
+++ components/ui/CustomFormField.tsx
@@ -11,23 +11,90 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Control } from "react-hook-form";
+import { FormFieldType } from "../forms/PatientForm";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+import Image from "next/image";
 
-interface CustomProps {
+interface CustomFormField {
   control: Control<any>;
+  fieldType: FormFieldType;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSekelton?: (field: any) => React.ReactNode;
 }
-const CustomFormField = ({ control }: CustomProps) => {
+
+const RenderField = ({
+  field,
+  props,
+}: {
+  field: any;
+  props: CustomFormField;
+}) => {
+  const { fieldType, placeholder, iconSrc, iconAlt } = props;
+  switch (fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400">
+          {iconSrc && (
+            <Image
+              src={iconSrc}
+              height={24}
+              width={24}
+              alt={iconAlt || "icon"}
+              className="ml-2"
+            />
+          )}
+          <FormControl>
+            <Input
+              {...field}
+              placeholder={placeholder}
+              className="shad-input border-0"
+            />
+          </FormControl>
+        </div>
+      );
+    case FormFieldType.PHONE_INPUT:
+      console.log(field)
+      return (
+        <FormControl>
+          <PhoneInput
+            defaultCountry="US"
+            placeholder={placeholder}
+            international
+            value={field.value}
+            onChange={field.onChange}
+            className="input-phone"
+          />
+        </FormControl>
+      );
+    default:
+      break;
+  }
+};
+
+const CustomFormField = (props: CustomFormField) => {
+  const { control, fieldType, name, label } = props;
   return (
     <FormField
       control={control}
-      name="username"
+      name={name}
       render={({ field }) => (
-        <FormItem>
-          <FormLabel>Username</FormLabel>
-          <FormControl>
-            <Input placeholder="shadcn" {...field} />
-          </FormControl>
-          <FormDescription>This is your public display name.</FormDescription>
-          <FormMessage />
+        <FormItem className="flex-1">
+          {fieldType != FormFieldType.CHECKBOX && label && (
+            <FormLabel>{label}</FormLabel>
+          )}
+
+          <RenderField field={field} props={props} />
+
+          <FormMessage className="shad-error" />
         </FormItem>
       )}
     />

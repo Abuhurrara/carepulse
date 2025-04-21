edca277c8e252c5ce8ac11d4f10095b38462b631
Abuhurrara <abuhurrara1093@gmail.com>
2025-04-22 02:56:05 +0500

    add validation

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -6,7 +6,7 @@ import { z } from "zod";
 import { Form, FormControl } from "@/components/ui/form";
 import SubmitButton from "../SubmitButton";
 import { useState } from "react";
-import { userFormValidation } from "@/lib/validation";
+
 import { useRouter } from "next/navigation";
 import { createUser } from "@/lib/actions/patient.actions";
 import { FormFieldType } from "./PatientForm";
@@ -17,13 +17,14 @@ import { SelectItem } from "../ui/select";
 import Image from "next/image";
 import CustomFormField from "../CustomFormField";
 import FileUploader from "../FileUploader";
+import { UserFormValidation } from "@/lib/validation";
 
 const RegisterForm = ({ user }: { user: User }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
-  const form = useForm<z.infer<typeof userFormValidation>>({
-    resolver: zodResolver(userFormValidation),
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
       name: "",
       email: "",
@@ -35,7 +36,7 @@ const RegisterForm = ({ user }: { user: User }) => {
     name,
     email,
     phone,
-  }: z.infer<typeof userFormValidation>) {
+  }: z.infer<typeof UserFormValidation>) {
     setIsLoading(true);
 
     try {

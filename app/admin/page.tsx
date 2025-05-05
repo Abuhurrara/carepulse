bb088dfd83c215735fd9ca444c3a38da5e99aabf
Abuhurrara <abuhurrara1093@gmail.com>
2025-05-06 00:34:16 +0500

    add table

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,13 +1,16 @@
-import StatCard from "@/components/StatCard";
-import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import StatCard from "@/components/StatCard";
+import { columns, Payment } from "@/components/table/columns";
+import { DataTable } from "@/components/table/DataTable";
+import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
 
 const Admin = async () => {
   const appointments = await getRecentAppointmentList();
 
   console.log(appointments);
+  
 
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
@@ -51,6 +54,8 @@ const Admin = async () => {
             icon="/assets/icons/cancelled.svg"
           />
         </section>
+
+        <DataTable columns={columns} data={appointments.documents} />
       </main>
     </div>
   );

2c5b8d27e82a1b73b1b0e21f2a68f571fc90f10f
Abuhurrara <abuhurrara1093@gmail.com>
2025-05-11 01:02:32 +0500

    added sms integration

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -5,8 +5,9 @@ import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
   databases,
+  messaging,
 } from "../appwrite.config";
-import { parseStringify } from "../utils";
+import { formatDateTime, parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite.types";
 import { revalidatePath } from "next/cache";
 
@@ -81,6 +82,21 @@ export const getRecentAppointmentList = async () => {
   }
 };
 
+// Sending SMS notification.
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId]
+    );
+    return parseStringify(message);
+  } catch (error) {
+    console.error("An error occurred while sending sms:", error);
+  }
+};
+
 export const updateAppointment = async ({
   appointmentId,
   userId,
@@ -88,7 +104,6 @@ export const updateAppointment = async ({
   type,
 }: UpdateAppointmentParams) => {
   try {
-    console.log("appoinddddddtmentId", appointmentId);
     const updatedAppointment = await databases.updateDocument(
       DATABASE_ID!,
       APPOINTMENT_COLLECTION_ID!,
@@ -99,7 +114,20 @@ export const updateAppointment = async ({
     if (!updatedAppointment) {
       throw new Error("Error updating appointment");
     }
-    // TODO: sms message
+
+    const smsMessage = `
+   Hi it's CarePulse.
+   ${
+     type === "schedule"
+       ? `Your appointment has been scheduled for ${formatDateTime(
+           appointment.schedule!
+         )}`
+       : `we regret to inform you that your appointment has been cancelled for the following
+    reason: ${appointment.cancellationReason}.`
+   }
+   `;
+
+    await sendSMSNotification(userId, smsMessage);
 
     revalidatePath("/admin");
     return parseStringify(updatedAppointment);
